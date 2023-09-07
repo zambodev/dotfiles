dbdc7e00254ad0187aefa43966f4ff39f865ee5e
zambodev <thomaszambelli01@gmail.com>
2023-09-07 20:36:40 +0200

    Update

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env sh
+
+# Update
+sudo apt-get update -y && sudo apt-get upgrade -y </dev/null
+printf "Update finished!\n"
 
 # Install
-sudo apt install -y zsh vim neofetch gcc make nasm python3 python3-pip
+sudo apt install -y curl zsh vim neofetch gcc make nasm python3 python3-pip </dev/null
+printf "Packages installed!\n"
 
 # Setup zsh
-chsh -s $(which zsh)
-cp .zshrc ~/.zshrc
+sudo curl -fsSL https://raw.githubusercontent.com/zambodev/dotfiles/master/.zshrc > ~/.zshrc
+printf "Zsh setup done!\n"
 
 # Setup vim
-cp .vimrc ~/.vimrc
+sudo curl -fsSL https://raw.githubusercontent.com/zambodev/dotfiles/master/.vimrc > ~/.vimrc
+printf "Vim setup done!\n"
 
-
-
+sudo chsh -s $(which zsh)
