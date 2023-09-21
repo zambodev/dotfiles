69194596d9b60a8c5c0f0f9d66f8e91a4dbea1bf
Zambo <thomaszambelli01@gmail.com>
2023-09-21 09:13:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+# Update
+sudo apt-get update -y && sudo apt-get upgrade -y </dev/null
+printf "Update finished!\n"
+
+# Install
+sudo apt install -y curl vim neofetch gcc g++ make nasm python3 python3-pip </dev/null
+printf "Packages installed!\n"
 
 # Setup bash
 sudo curl -fsSL https://raw.githubusercontent.com/zambodev/dotfiles/master/.bashrc > ~/.bashrc
