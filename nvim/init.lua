0fbcaa4b975847e89c1902ea4e7d50aaad7cfa0a
zambodev <thomaszambelli01@gmail.com>
2026-06-07 15:31:18 +0200

    New single init.lua neovim 0.12 config + lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,108 +1,238 @@
-require("config.lazy")
+-- Settings --------------------------------
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
--- Config
-vim.opt.clipboard = 'unnamedplus'
-vim.wo.number = true
-vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.number = true
+vim.opt.relativenumber = false
 vim.opt.signcolumn = "yes"
-vim.opt.expandtab = true  
-vim.opt.tabstop = 4      
-vim.opt.shiftwidth = 4   
-vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.opt.updatetime = 250
 
--- diagnostics
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(event)
-    vim.keymap.set('n', '<leader>i', function()
-      vim.lsp.buf.hover {
-        border = 'rounded',
-      }
-    end, { buffer = event.buf })
-  end,
+-- Indentation
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
+
+-- lazy --------------------------------
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  -- Colorscheme: One Dark Pro
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000,
+    config = function()
+      require("onedarkpro").setup({
+        options = {
+          highlight_inactive_windows = true,
+        }
+      })
+      vim.cmd("colorscheme onedark")
+    end,
+  },
+
+  -- blink
+  {
+    'Saghen/blink.cmp',
+    dependencies = 'rafamadriz/friendly-snippets',
+    version = '*',
+    opts = {
+      keymap = { preset = 'default' },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+    },
+  },
+
+  -- Telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("telescope").setup({})
+    end,
+  },
+
+  -- File tree
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup({
+        view = {
+          width = 30,
+          side = "left",
+        },
+      })
+    end,
+  },
+
+  -- Git signs
+  {
+    "lewis6991/gitsigns.nvim",
+    config = true,
+  },
+
+  -- Git diff
+  {
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = true,
+  },
+
+  -- Git
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("neogit").setup({
+        kind = "vsplit",
+        integrations = { diffview = true },
+      })
+    end,
+  },
+
+  -- Multi-cursor Support
+  {
+    "mg979/vim-visual-multi",
+    lazy = false,
+    init = function()
+      -- Configure custom multi-cursor trigger keys
+      vim.g.VM_maps = {
+        ["Find Under"]         = "gb",       -- Swap "gb" with "<leader>n" if you prefer space+n
+        ["Visual Find Under"]  = "gb",       -- For selecting matches while in visual mode
+      }
+    end,
+  },
 })
 
-vim.diagnostic.config({
-  virtual_text = false,      -- no inline text
-  virtual_lines = false,     -- no virtual lines
-  underline = true,          -- optional: underline problematic code
-  update_in_insert = false,  -- don't show diagnostics while typing
-  severity_sort = true,
-})
-
-vim.o.updatetime = 250  -- how long in ms before CursorHold triggers
-vim.api.nvim_create_autocmd("CursorHold", {
+-- Native Treesitter Highlighting (Built into Neovim 0.12+) ----------------
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "c", "cpp", "rust", "lua", "vim", "vimdoc", "query" },
   callback = function()
-    -- Show diagnostics only if there are any
-    local opts = {
-      focusable = false,
-      border = "rounded",
-      source = "always",    -- show source (like linter)
-      prefix = "● ",        -- optional bullet
-    }
-    vim.diagnostic.open_float(nil, opts)
+    vim.treesitter.start()
   end,
 })
 
--- Remaps
-vim.g.leader = " "
-vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float )
-vim.keymap.set("n", "<leader>w", ":w<cr>")
-vim.keymap.set("n", "<leader>o", ":noh")
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr, desc = "[R]e[n]ame" })
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr, desc = "[C]ode [A]ction" })
-vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "[G]oto [D]efinition" })
-vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, { buffer = bufnr, desc = "[G]oto [D]eclaration" })
-vim.keymap.set("n", "<leader>gI", vim.lsp.buf.implementation, { buffer = bufnr, desc = "[G]oto [I]mplementation" })
+-- LSP --------------------------------
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
--- LSP
-vim.lsp.enable({
-	-- C/C++
-	"clangd"
+-- Rust
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "rust",
+  callback = function(args)
+    local bufnr = args.buf
+    vim.lsp.start({
+      name = "rust-analyzer",
+      cmd = { "rust-analyzer" },
+      capabilities = capabilities,
+      root_dir = vim.fs.root(0, { "Cargo.toml", ".git" }),
+      settings = {
+        ["rust-analyzer"] = {
+          checkOnSave = true,
+          check = {
+            command = "clippy",
+          },
+        },
+      },
+    })
+  end,
 })
 
--- Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files)
-vim.keymap.set('n', '<leader>gf', builtin.git_files)
-vim.keymap.set('n', '<leader>gr', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
-vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-vim.keymap.set("n", "<leader>j", "<C-w>j")
-vim.keymap.set("n", "<leader>k", "<C-w>k")
-vim.keymap.set("n", "<leader>h", "<C-w>h")
-vim.keymap.set("n", "<leader>l", "<C-w>l")
-vim.keymap.set("n", "<leader>J", "<C-w>J")
-vim.keymap.set("n", "<leader>K", "<C-w>K")
-vim.keymap.set("n", "<leader>H", "<C-w>H")
-vim.keymap.set("n", "<leader>L", "<C-w>L")
-
-vim.keymap.set("n", "<S-h>", "gt")
-vim.keymap.set("n", "<S-l>", "gT")
-
--- Autocomplete
-local cmp = require("cmp")
-local luasnip = require("luasnip")
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<C-n>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-p>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- enter to confirm
-  }),
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "buffer" },
-    { name = "path" },
-    { name = "luasnip" },
-  }),
+-- C++
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "c", "cpp", "h", "hpp" },
+  callback = function(args)
+    local bufnr = args.buf
+    vim.lsp.start({
+      name = "clangd",
+      cmd = { 
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--header-insertion=iwyu",
+        "--completion-style=detailed",
+        "--function-arg-placeholders",
+        "--fallback-style=llvm"
+      },
+      capabilities = capabilities,
+      init_options = {
+        fallbackFlags = { "-std=c++26" },
+      },
+      root_dir = vim.fs.root(0, { 
+        "compile_commands.json", 
+        "compile_flags.txt", 
+        ".clangd", 
+        ".clang-tidy", 
+        ".clang-format", 
+        ".git" 
+      }),
+    })
+  end,
 })
+
+-- Diagnostics --------------------------------
+vim.diagnostic.config({
+  virtual_text = { prefix = "●" },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
+
+-- Error popup shortcut
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show line diagnostics popup" })
+
+-- Error popup hover
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    vim.diagnostic.open_float(nil, { focusable = false, scope = "cursor" })
+  end,
+})
+
+-- LSP hover docs
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local bufnr = args.buf
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP Hover Documentation" })
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "Go to Definition" })
+  end,
+})
+
+-- Keybinds --------------------------------
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Telescope Find Files" })
+vim.keymap.set("n", "<leader>g", builtin.live_grep, { desc = "Telescope Live Grep (Search Text)" })
+
+-- File Tree
+vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>", { desc = "Toggle File Tree" })
+
+-- Git
+vim.keymap.set("n", "<leader>gs", ":Neogit<CR>", { desc = "Git: Open Source Control Sidebar Status" })
+vim.keymap.set("n", "<leader>gd", ":DiffviewOpen<CR>", { desc = "Git: View File Diff Splits" })
+vim.keymap.set("n", "<leader>gh", ":DiffviewFileHistory %<CR>", { desc = "Git: View Current File History" })
+vim.keymap.set("n", "<leader>gc", ":DiffviewClose<CR>", { desc = "Git: Close Diff View Windows" })
+
+-- Keymap legend
+vim.keymap.set("n", "<leader>?", builtin.keymaps, { desc = "Search Keyboard Shortcuts" })
 
